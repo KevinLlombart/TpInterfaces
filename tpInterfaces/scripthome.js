b661d1f3b144de06cab59d1daeaa264f3cf01f20
Florencia Carvajal <florcar1998@gmail.com>
2024-06-28 13:16:19 -0300

    Home

---
+++ tpInterfaces/scripthome.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.your-class').slick({
+    
+        adaptiveHeight: true,
+        arrows:true,
+        prevArrow: '.arrow-prev',
+        nextArrow: '.arrow-next'
+
+
+    });
+  });
